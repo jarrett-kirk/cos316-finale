2ce7e706a283e169eb843cba7f61c0060a073022
Jack Kenkel <jkenkel@princeton.edu>
2023-12-13 12:59:43 -0500

    Co-authored-by: jarrettkirk1 <jarrettkirk1@users.noreply.github.com>

--- client/main.go
+++ client/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+)
+
+func main() {
+	// var eth layers.Ethernet
+	// var ip4 layers.IPv4
+	// var ip6 layers.IPv6
+	// var tcp layers.TCP
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{}
+	gopacket.SerializeLayers(buf, opts,
+		&layers.Ethernet{},
+		&layers.IPv4{
+			SrcIP:    net.IPv4(120, 64, 20, 10),
+			DstIP:    net.IPv4(190, 20, 69, 11),
+			Protocol: layers.IPProtocolTCP,
+		},
+		&layers.TCP{},
+		gopacket.Payload([]byte{1, 2, 3, 4}))
+	packetData := buf.Bytes()
+	fmt.Println(packetData)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module finalproject
 
 go 1.21.1
+
+require github.com/google/gopacket v1.1.19 // indirect
