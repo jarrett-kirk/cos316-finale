8b717bb94354bcb0384ec380e9403f468fc2b97b
Jack Kenkel <jkenkel@princeton.edu>
2023-12-13 13:00:00 -0500

    commit

--- iptable/iptable.go
+++ iptable/iptable.go
@@ -5,6 +5,12 @@ type Rule struct {
 	// target
 	target string
 	// matches
+	SrcIP      string
+	DstIP      string
+	IPProtocol string
+	DstPort    string
+	SrcPort    string
+	ethernet   string
 }
 
 // chain structure
@@ -14,6 +20,16 @@ type Chain struct {
 }
 
 // Table structure contains a set of chains
+type Table struct {
+	name   string
+	chains []*Chain
+}
+
+// function initialize IPtable
+func NewIPTable() *Table {
+	filter := new(Table)
+	return filter
+}
 
 // function to add a chain
 func addChain() {
